06027d6dcbce0b920df0e45ab51757d1f1d69954
Constan4 <162689199+Constan4@users.noreply.github.com>
2024-04-02 13:51:00 +0200

    Update pru1.java

--- githubpru1/src/githubpru1/pru1.java
+++ githubpru1/src/githubpru1/pru1.java
@@ -4,6 +4,7 @@ public class pru1 {
 	public static void main(String[] args) {
 		System.out.println("Primer comentario");
 		System.out.println("Segundo comentario");
+		System.out.println("Tercer comentario");
 	}
 
 }
